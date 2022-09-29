d0efa9c7d3cfab550e5810e07848713a3d2acd56
Richard <richardmixihe@gmail.com>
2022-09-29 20:45:33 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -186,7 +186,97 @@ classSalaryChart();
 
 
 
+//---------------------------柱状图：班级每组薪资
+function histogram() {
+  let myChart = echarts.init(document.querySelector('#lines'));//返回文档中匹配指定 CSS选择器的一个元素。!!注意仅仅返回匹配指定选择器的第一个元素)
 
+  option = {
+    grid:{
+      left:70,
+      top:30,
+      right:30,
+      bottom:50,
+      
+    },
+    xAxis: {
+      color:'#ccc',
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      axisLine:{
+        lineStyle:{
+           type:'dashed',
+        },
+      },
+      axisLabel: {
+          color: '#999'
+        },
+    
+    },
+    yAxis: {
+      type: 'value',
+      splitLine:{
+        lineStyle:{
+          type:'dashed'
+        }
+      }
+    },
+    color: [
+        {
+          type: 'linear',
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: '#34D39A' // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: 'rgba(52,211,154,0.2)' // 100% 处的颜色
+            }
+          ],
+          global: false // 缺省为 false
+        },
+        {
+          type: 'linear',
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: '#499FEE' // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: 'rgba(73,159,238,0.2)' // 100% 处的颜色
+            }
+          ],
+          global: false // 缺省为 false
+        }
+      ],
+    series: [
+      
+      {
+        data: [12200, 17932, 13901, 13934, 21290, 23300, 13300, 13320],
+        type: 'bar',
+        name: '期望薪资'
+      },
+      
+      {
+        data: [22820, 19932, 16901, 15934, 31290, 13300, 14300, 18320],
+        type: 'bar',
+        name: '就业薪资'
+      }
+    ],
+    tooltip: {},
+  };
+  myChart.setOption(option)
+}
+histogram();
 
 
 
@@ -196,6 +286,106 @@ classSalaryChart();
 
 
 //---------------------------饼图：全学科薪资走势
+function salary() {
+  let myChart = echarts.init(document.querySelector('#gender'));//返回文档中匹配指定 CSS选择器的一个元素。!!注意仅仅返回匹配指定选择器的第一个元素)
+  let option ={
+
+      title:[
+        {
+            text: '男女薪资分布',
+            left: 10, // 10 表示10px
+            top: 10,
+            textStyle: {
+              fontSize: 16
+            }
+          },
+          {
+            text: '男生',
+            left: '48%', // 10 表示10px
+            top: '50%',
+            textStyle: {
+              fontSize: 12
+            }
+          },
+          {
+            text: '女生',
+            left: '48%', // 10 表示10px
+            top: '95%',
+            textStyle: {
+              fontSize: 12
+            }
+          }
+          ],
+          color: ['#FDA224', '#5097FF', '#3ABCFA', '#34D39A'],
+      tooltip: {
+        trigger: 'item'
+      },
+      // legend: {
+      //   top: '5%',
+      //   left: 'center'
+      // },
+      series: [
+        {
+          name: '男生薪资分布',
+
+          type: 'pie',
+          center: ['50%', '30%'],
+          radius: ['20%', '40%'],
+          avoidLabelOverlap: true,
+          label: {
+            show: true,
+            // position: 'center'
+          },
+          emphasis: {
+            label: {
+              show: true,
+              // fontSize: '40',
+              fontWeight: 'bold'
+            }
+          },
+          // labelLine: {
+          //   show: false
+          // },
+          data: [
+            { value: 1048, name: '1万以下' },
+            { value: 235, name: '1万-2万' },
+            { value: 580, name: '1.5万-2万' },
+            { value: 484, name: '2万以上' }
+          ],
+        },
+        {
+          name: '女生工资分布',
+          type: 'pie',
+          radius: ['20%', '40%'],
+          center: ['50%', '75%'],
+          avoidLabelOverlap: true,
+          label: {
+            show: true,
+            // position: 'center'
+          },
+          emphasis: {
+            label: {
+              show: true,
+              // fontSize: '40',
+              fontWeight: 'bold'
+            }
+          },
+          // labelLine: {
+          //   show: true
+          // },
+          data: [
+            { value: 1048, name: '1万以下' },
+          { value: 235, name: '1万-2万' },
+          { value: 580, name: '1.5万-2万' },
+          { value: 484, name: '2万以上' }
+          ],
+        }
+      ]
+
+  }
+  myChart.setOption(option)
+}
+salary()
 //---------------------------地图：
 
 function mapChart() {
@@ -216,7 +406,7 @@ function mapChart() {
             type:'map',
             map:'china',
             itemStyle:{
-                areaColor:'blue',
+                areaColor:'#E0FFFF',
                 
             }
         }]

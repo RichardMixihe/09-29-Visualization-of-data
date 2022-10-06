24058e5dc68b5f024477bbdcd75f7cb3783bc557
Richard <richardmixihe@gmail.com>
2022-10-06 16:24:45 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -15,25 +15,22 @@ axios.defaults.baseURL = 'http://ajax-api.itheima.net'
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
 // 2.配置请求拦截器（目的是加请求头）
 // 3.配置相应拦截器（判断token是否过期）
- // 添加响应拦截器
-axios.interceptors.response.use(
-  function (response) {
-    // 2xx 范围内的状态码都会触发该函数。
-    // 对响应数据做点什么
-    return response
-  },
-  function (error) {
-    // console.dir(error)
-    if (error.response) {
-      // 先确保能够得到响应结果
-      if (error.response.data.message === 'token验证失败') {
-        // 说明token过期了，或者使用了假token
-        localStorage.removeItem('token')
-        location.href = './login.html'
-      }
-    }
-    // 超出 2xx 范围的状态码都会触发该函数。
-    // 对响应错误做点什么
-    return Promise.reject(error)
+ // axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+
+// 另外，使用请求拦截器也可以，代码如下
+// 3. 配置响应拦截器（判断token是否过期）
+axios.interceptors.response.use(function (response) {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // console.dir(error)
+  if (error.response.status === 401) {
+    // 说明token过期了，总之token不能用了
+    localStorage.removeItem('token')
+    location.href = './login.html' // JS中的相对路径和 js 文件在哪里无关；和使用js的html有关系
   }
-)
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});

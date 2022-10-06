22fd2c6631f2d9eef99ca28bd863daaadc54ee8b
Richard <richardmixihe@gmail.com>
2022-10-06 16:02:51 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -15,3 +15,25 @@ axios.defaults.baseURL = 'http://ajax-api.itheima.net'
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
 // 2.配置请求拦截器（目的是加请求头）
 // 3.配置相应拦截器（判断token是否过期）
+ // 添加响应拦截器
+axios.interceptors.response.use(
+  function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response
+  },
+  function (error) {
+    // console.dir(error)
+    if (error.response) {
+      // 先确保能够得到响应结果
+      if (error.response.data.message === 'token验证失败') {
+        // 说明token过期了，或者使用了假token
+        localStorage.removeItem('token')
+        location.href = './login.html'
+      }
+    }
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error)
+  }
+)
